c53861b6eb3a5c48fc7594b79eb6431b8bac7f90
Deusdete Ferreira <deuzinh2010@gmail.com>
2021-09-24 21:47:40 -0300

    Alteração no index.js

--- index.js
+++ index.js
@@ -84,5 +84,5 @@ server.delete('/alunos/:id', (request, response) => {
 })
 
 server.listen(3000, () => {
-  console.log('Server rodando na porta 3000 🚀')
+  console.log('Server rodando na porta => 3000 🚀')
 })
